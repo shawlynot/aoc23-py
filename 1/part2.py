bc6f45b9482007f262395effc9eb9e0ce79de443
shawlynot <88781957+shawlynot@users.noreply.github.com>
2023-12-02 23:46:49 +0000

    aoc1 part2

--- 1/part2.py
+++ 1/part2.py
@@ -1,19 +1,23 @@
 import re
-numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-numbers_as_re = "|".join(numbers)
+
+numbers_as_re = "|".join(["on(?=e)", "tw(?=o)", "thre(?=e)", "four", "fiv(?=e)", "six", "seve(?=n)", "eigh(?=t)", "nin(?=e)"])
+numbers = ["on", "tw", "thre", "four", "fiv", "six", "seve", "eigh", "nin"]
 
 def get_digit(digit: str) -> str: 
-    if re.search(f"({numbers_as_re})", digit) is not None:
-        return str(numbers.index(digit) + 1)
-    return digit
+    if digit.isdigit():
+        return digit
+    return str(numbers.index(digit) + 1)
+   
+def main(): 
+    with open("_input") as input:
+        total = 0
+        for line in input.readlines():
+            digits = re.findall(f"(\d|{numbers_as_re})", line)
+            first_digit = get_digit( digits[0])
+            second_digit = get_digit( digits[-1])
+            number_str = "".join([first_digit, second_digit])
+            total += int(number_str)
+        print(total)
 
-with open("_input") as input:
-    total = 0
-    for line in input.readlines():
-        digits = re.findall(f"(\d|{numbers_as_re})", line)
-        first_digit = get_digit( digits[0])
-        second_digit = get_digit( digits[-1])
-        number_str = "".join([first_digit, second_digit])
-        total += int(number_str)
-print(total)
-
+if (__name__ == "__main__"):
+    main()
